5bc7c8dfad77289519015ed6cdc296703e2689d5
Aayushi Kumari <72645267+AayushiKumari@users.noreply.github.com>
2023-10-31 02:14:39 +0530

    Add files via upload

---
+++ pangram_check.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPangram(input)) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String input) {
+        
+        Set<Character> uni_letters = new HashSet<>();
+        input = input.toLowerCase();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                uni_letters.add(ch);
+            }
+        }
+
+        return uniqueLetters.size() == 26;
+    }
+}
